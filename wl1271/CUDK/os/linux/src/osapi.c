12f4dde4e54193c0ee528b627a28cba46304edf5
daedae <daeyoung1112@gmail.com>
2012-12-22 03:15:39 +0900

    Revert "fix wifi compile"
    This reverts commit 66fe0b49f98f16a5a6b10cd535850e3e9246acc5.

--- wl1271/CUDK/os/linux/src/osapi.c
+++ wl1271/CUDK/os/linux/src/osapi.c
@@ -95,7 +95,7 @@ VOID os_error_printf(S32 debug_level, const PS8 arg_list ,...)
     va_end(ap);
 
     /* print the message */
-    fprintf(stderr, "%s", (char *)msg);
+    fprintf(stderr, (char *)msg);
 
 #ifdef OS_CLI_LOG_TO_FILE
     ftmp = fopen(file_name, "a");
@@ -152,7 +152,7 @@ S32 os_sprintf(PS8 str, const PS8 arg_list, ...)
     vsprintf((char*)msg, (char*)arg_list, ap);
     va_end(ap);
 
-    return sprintf((char*)str, "%s", (char*)msg);
+    return sprintf((char*)str, (char*)msg);
 }
 
 /****************************************************************************************
@@ -166,7 +166,7 @@ RETURN:
 *****************************************************************************************/
 S32 os_Printf(const PS8 buffer)
 {
-    return printf("%s", (char*)buffer);
+    return printf((char*)buffer);
 }
 
 
@@ -331,7 +331,7 @@ S32 os_sscanf(const PS8 str, const PS8 arg_list, ...)
     vsprintf((char*)msg, (char*)arg_list, ap);
     va_end(ap);
 
-    return sscanf((char*)str, "%s", (char*)msg);
+    return sscanf((char*)str, (char*)msg);
 }
 
 /************************************************************************
